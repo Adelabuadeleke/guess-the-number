b1ecd21d1d9279e7ff7ae7bfd989893572b3b264
Kamal Adelabu <63759015+Adelabuadeleke@users.noreply.github.com>
2022-06-03 20:39:22 +0000

    Local storage functionality added, all game functions, tested and working fine...🧑🏾‍💻

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-const username = prompt ("username");
+//importing dependencies
+const {LocalStorage} = require("node-localstorage");
+let localStorage = new LocalStorage('./scratch');
 
+//get user name
+let username = localStorage.getItem('username');
+if(username == null) {
+  username = prompt ("username");
+}
+
+
+//setting parameters
+localStorage.setItem('username', username);
+let rangeVal = [];
+let x;
+
+//stage function
+const getStage = () => {
+ let result = localStorage.getItem('stage');
+if (result == null) {
+  result = 1;
+}
+  return result;  
+} 
+
+let stage = getStage();
+
+//range function
 function* range(start, end) {
 for (let h = start; h <= end; h++) {
 yield h;
 }
 }
 
-let rangeVal = [];
-let x;
-let defaultStage = 1;
-let stage = defaultStage;
-
-
-if(stage == 1) {
-  x = 2
+//determine range limit
+if(stage) {
+  x = Number(stage) + 1
 }
+
+//push range values into array
 for (h of range(1,x)) {
 rangeVal.push(h)
 }
 
-
+//random selection
 let botSelection = Math.floor(Math.random() * rangeVal .length)
 
+//get user selection
 const userSelection = prompt("guess number btw range 1 - " + x)
 
 
@@ -29,15 +53,21 @@ if(botSelection == 0){
   botSelection = 1;
 }
 
-console.log(rangeVal);
-console.log(botSelection);
-console.log(userSelection);
+//printing out game details
+console.log(`stage: ${stage}`);
+console.log(`range options: ${rangeVal}`);
+console.log(`botSelection: ${botSelection}`);
+console.log(`yourSelection: ${userSelection}`);
 
+
+let playerName = localStorage.getItem('username');
+
+//game result function
 if(Number(botSelection) == Number(userSelection)) {
-console.log(`${username} you won stage 1!🕺`)
+console.log(`congrats ${playerName} you won this stage!🕺`);
+  localStorage.setItem('stage', `${Number(stage) + 1}`);
 } else {
   console.log("you lost, game over!🥲")
+  localStorage.removeItem("stage");
+  localStorage.removeItem("username");
 }
-
-
-
